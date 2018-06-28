c7afacab52553f82a1c7c5ec9bfb266ee024293b
Ville M. Vainio <ville.vainio@basware.com>
2018-06-28 19:20:24 +0300

    ensure dir for unzip output

--- zipget.py
+++ zipget.py
@@ -17,8 +17,9 @@ import pprint
 MAX_RETRIES = 5
 BLOCK_SIZE = 10 * 1024 * 1024
 
+
 def stream_to_file(to_file_name, from_file_obj):
-    fd, tmpfile_name = tempfile.mkstemp(dir = os.path.dirname(to_file_name))
+    fd, tmpfile_name = tempfile.mkstemp(dir=os.path.dirname(to_file_name))
     print("to", tmpfile_name)
     try:
         while 1:
@@ -30,16 +31,20 @@ def stream_to_file(to_file_name, from_file_obj):
         os.close(fd)
         os.rename(tmpfile_name, to_file_name)
 
+
 process_exit_code = 0
 
 _report_log = []
 
+
 def report_ok(s):
     _report_log.append(s)
+
 
 def report_flush():
     out = "\n".join("zipget: " + l for l in _report_log)
     print(out)
+
 
 def fetch_url(url, tgt):
     retries = MAX_RETRIES
@@ -48,7 +53,7 @@ def fetch_url(url, tgt):
 
         try:
             content = urlopen(url)
-        except HTTPError,e:
+        except HTTPError, e:
             if e.code >= 500:
                 retries -= 1
                 print("Fail", url, "retry", retries, e)
@@ -59,8 +64,8 @@ def fetch_url(url, tgt):
             else:
                 process_exit_code = 1
                 raise
-        except Exception,e:
-            print("Fail with exception",e)
+        except Exception, e:
+            print("Fail with exception", e)
             process_exit_code = 2
             raise
 
@@ -73,26 +78,37 @@ def file_name_from_url(url):
     fpart = "".join(ch for ch in url.rsplit("/")[-1] if ch.isalnum())
     return hash + "_" + fpart
 
+
 def unzip_to(fname, tgt):
-    subprocess.check_call(["unzip",  "-o", "-q", fname, "-d", tgt])
+    ensure_dir_for(tgt)
+
+    subprocess.check_call(["unzip", "-o", "-q", fname, "-d", tgt])
     report_ok("unzipped %s <- %s" % (tgt, fname))
 
-def path_from_config(config,path):
+
+def path_from_config(config, path):
     if os.path.isabs(path):
         return path
     return os.path.normpath(os.path.join(config['root'], path))
+
 
 def run_exe(target_path, args):
     parts = [target_path] + args
     print(">", target_path, args)
     subprocess.check_call(parts)
 
+
+def ensure_dir(dname):
+    if not os.path.isdir(dname):
+        os.makedirs(dname)
+
+
 def ensure_dir_for(pth):
     dname = os.path.dirname(pth)
     if not dname:
         return
-    if not os.path.isdir(dname):
-        os.makedirs(dname)
+    ensure_dir(dname)
+
 
 def handle_fetch(fetch, config):
     url = fetch['url']
@@ -114,6 +130,7 @@ def handle_fetch(fetch, config):
     if runargs:
         run_exe(targetpath, runargs)
 
+
 def created_temp_dir():
     """ creates archive directory in temp (used of none of specified exist) """
     pth = os.path.join(tempfile.gettempdir(), "zipget")
@@ -121,6 +138,7 @@ def created_temp_dir():
         print("Warning: none of archive dirs existed, creating", pth)
         os.makedirs(pth)
     return pth
+
 
 def accept_frag(frag, tags):
     # user didn't specify tags -> install everything
@@ -132,14 +150,15 @@ def accept_frag(frag, tags):
     # user specified tags -> install only if overlapping tags
     return len(set(tags).intersection(set(frag['tags']))) > 0
 
+
 def handle_recipe(fname, args):
     r = json.load(open(fname))
 
-    config = {
-        "root": os.path.abspath(os.path.dirname(fname))
-    }
+    config = {"root": os.path.abspath(os.path.dirname(fname))}
     archive_dirs = r['config']['archive']
-    archive_tries = [f for f in archive_dirs if os.path.isdir(path_from_config(config, f) )]
+    archive_tries = [
+        f for f in archive_dirs if os.path.isdir(path_from_config(config, f))
+    ]
     if len(archive_tries) == 0:
         archive = created_temp_dir()
     else:
@@ -164,15 +183,21 @@ def handle_recipe(fname, args):
         print("Zipget failed with exit code", process_exit_code)
         sys.exit(process_exit_code)
 
+
 def main(args):
     """ Handle and dispatch args. Can be used as 'api' """
     p = ArgumentParser()
     p.add_argument('recipe', nargs=1, help="Recipe (json) file to run")
-    p.add_argument('tags', nargs="*", metavar="tag", help="Tags for parts of recipe to run")
+    p.add_argument(
+        'tags',
+        nargs="*",
+        metavar="tag",
+        help="Tags for parts of recipe to run")
     p.add_argument('-v', action="store_true", help="Verbose output")
     parsed = p.parse_args(args)
     recipe = parsed.recipe[0]
     handle_recipe(recipe, parsed)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
